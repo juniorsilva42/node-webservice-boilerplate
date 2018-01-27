6123a49160c60ec5d4ca6d62e9d722c95109de97
jsiilva <jsiilva@outlook.com.br>
2018-01-27 14:50:02 -0300

    adicionando os primeiros testes

---
+++ src/services/mysql/tests/categories.test.js
@@ -0,0 +1,15 @@
+// create table categories (id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id));
+
+import test from 'ava'
+
+const { connection, errorHandler } = require('./setup')
+
+const categories = require('../categories')({ connection, errorHandler });
+
+test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+test.after.always(t => connection.query('TRUNCATE TABLE categories'))
+
+test('Criação de uma categoria', async t => {
+  const result = await categories.save('Categoria-vindo-do-teste')
+  t.is(result.category.name, 'Categoria-vindo-do-teste')
+})
